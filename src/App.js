9ff4806724ddba4d21e71dc25e437acde350da3e
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-12 22:24:18 +0500

    add unais

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
+import InfoPanel from './components/InfoPanel';
+
 
 function App() {
   return (
     <div >
     <NavBar/>
+    <InfoPanel/>
     </div>
   );
 }
