a259dd5b82b4eb222bdfc92bea186c37fac10859
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-13 01:14:51 +0500

    add unais

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import FootNav from './components/FootNav'
 
 
 function App() {
-  const screenConfig = useState(0);
+  const screenConfig = useState(0); //screenconfig use to receive and send data to parent from child or etc
   return (
-    <div >
-    <NavBar/>
-    <InfoPanel screenConfig={screenConfig}/>
-    <FootNav/>
-    </div>
+    <div>
+		<NavBar />
+		<InfoPanel currentScreen={screenConfig[0]}/>
+		<FootNav screenConfig={screenConfig}/> 
+	</div>
   );
 }
 
--- src/components/FootNav.js
+++ src/components/FootNav.js
@@ -1,41 +1,37 @@
+  
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FolderIcon from '@material-ui/icons/Folder';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const useStyles = makeStyles({
   root: {
-    position: 'absolute',
-    bottom:0,
-    right:0,
-    left:0,
-
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      left: 0
   },
 });
 
-export default function FootNav(screenConfig) {
+export default function FootNav({screenConfig}) {
   const classes = useStyles();
-  const [value, setValue] = React.useState('recents');
-
- 
 
   return (
     <BottomNavigation
       value={screenConfig[0]}
       onChange={(event, newValue) => {
-        screenConfig[1](newValue);
+          console.log(newValue)
+          screenConfig[1](newValue);
       }}
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Global Stats" value="recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Country Stats" value="favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Graphs" value="nearby" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Contact" value="folder" icon={<FolderIcon />} />
+      <BottomNavigationAction label="Global Stats" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Country Stats" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Graphs" icon={<LocationOnIcon />} />
     </BottomNavigation>
   );
 }
