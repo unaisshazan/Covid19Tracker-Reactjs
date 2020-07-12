9a33b06d42a72f522d2489eb506d6276a4578364
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-12 21:53:13 +0500

    add unais

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
+
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
@@ -70,14 +70,9 @@ export default function NavBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
+        
       
-          </IconButton>
+   
           <Typography className={classes.title} variant="h6" noWrap>
             Covid19 Tracker
           </Typography>
