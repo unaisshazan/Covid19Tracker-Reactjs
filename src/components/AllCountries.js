445310057719e3f9b0a627b2d0e2a59f5efd7991
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-13 01:54:44 +0500

    add unais

--- src/components/AllCountries.js
+++ src/components/AllCountries.js
@@ -1,69 +1,67 @@
-import React ,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
-
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-
-    maxWidth:1000,
-    margin:'0 auto',
-    marginTop: '2%'
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  title: {
-
-    color:'rgb(0, 154, 255)',
-  },
-  list: {
-      color: 'rgb(27, 79, 114)', // text transform also work for uppercase
-  }
+    root: {
+        maxWidth: 1000,
+        margin: '0 auto',
+        marginTop: 50
+    },
+    title: {
+        textAlign: 'left'
+    },
+    table: {
+        height: 450,
+        overflowY: 'scroll',
+        display: 'block'
+    }
 }));
 
-export default function GlobalStats() {
+export default function AllCountries() {
     const [globalData, setGlobalData] = useState([{}]);
 
     useEffect(() => {
         async function getData() {
             const response = await fetch("https://api.thevirustracker.com/free-api?countryTotals=ALL");
             let data = await response.json();
-            
-            setGlobalData(Object.values(data.countryitems[0]));
+
+            setGlobalData(Object.values(Object.values(data.countryitems)[0]));
+            console.log(Object.values(Object.values(data.countryitems)[0]))
         }
         getData();
     }, [])
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-      {Object.keys(globalData[0]).map((key,ind)=>{ // always give unique key for dynamic content
-          //if you want array of all day use .key and for each and map use as same to fecth all the data in to website view
-        // these headings are fetching from api if you want to change it use .replace function .upper case replace(/_/g,' ')this means underscore will replace by space
-        return(
-            <Grid item xs={12} sm={4} key={ind}>
-            <Paper 
-            className={classes.paper}  // becasue global["active casses"] thats why we extract all the  keys with global data toUpperCase() will shift all the data to upper case
-            elevation={4}
-            ><h3 className={classes.title}>{key.replace(/_/g,' ').toUpperCase()}</h3>
-            <h3 className={classes.list}>{globalData[key]}</h3>
-            </Paper>
-          </Grid>
+    return (
+        <div className={classes.root}>
+            <table className={classes.table}>
+                <thead>
+                    <tr className={classes.title}>
+                        <th>Country Name</th>
+                        <th>Total Cases</th>
+                        <th>Active Cases</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {globalData.map((key, ind) => {
+                        return (
+                            <tr key={ind}>
+                                <th className={classes.title}>
+                                    {globalData[ind].title}
+                                </th>
+                                <td>
+                                    {globalData[ind].total_cases}
+                                </td>
+                                <td>
+                                    {globalData[ind].total_active_cases}
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
 
-        )
-      })}
-     
-    
-        </Grid>
-       
-    </div>
-  );
+        </div>
+    );
 }
-  
