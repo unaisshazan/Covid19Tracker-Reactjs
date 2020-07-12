378315051982473f70f673c94b42ab360d3c5600
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-12 22:30:10 +0500

    add unais

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -23,13 +23,13 @@ export default function InfoPanel() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>Grid 1</Paper>
+          <Paper className={classes.paper} elevation={4}>Grid 1</Paper>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>Grid 2</Paper>
+          <Paper className={classes.paper} elevation={4}>Grid 2</Paper>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>Grid 3</Paper>
+          <Paper className={classes.paper} elevation={4}>Grid 3</Paper>
         </Grid>
         </Grid>
        
